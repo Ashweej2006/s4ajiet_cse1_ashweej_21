9e49470d21be7b42ee11b6d2eec1ee1497cce908
Ashweej Pajithaya <ashweejpajithaya@gmail.com>
2026-05-04 11:57:46 +0530

    Optimize swap logic using XOR operation

--- swap.java
+++ swap.java
@@ -3,9 +3,9 @@ public class Main
 	public static void main(String[] args) {
 		int a=10;
 		int b=20;
-		a=a+b;
-		b=a-b;
-		a=a-b;
+		a=a^b;
+		b=a^b;
+		a=a^b;
 		System.out.println("a:"+a+" b:"+b);
 	}
 }

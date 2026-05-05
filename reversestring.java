0b01cb111c5ccde051618ff29106ad4c88d801a7
Ashweej Pajithaya <ashweejpajithaya@gmail.com>
2026-05-05 14:39:32 +0530

    Update reversestring.java

--- reversestring.java
+++ reversestring.java
@@ -1,4 +1,17 @@
 class Solution {
+    public static void main(String[] args) {
+    char[] s = {'h', 'e', 'l', 'l', 'o'};
+    new Solution().reverseString(s);
+
+   
+    StringBuilder sb = new StringBuilder();
+    for (char c : s) {
+        sb.append(c);
+    }
+    System.out.println(sb.toString()); 
+}
+    
+    
     public void reverseString(char[] s) {
 
         int start = 0;

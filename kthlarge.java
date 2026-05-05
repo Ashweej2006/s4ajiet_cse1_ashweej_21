8824cc32a4f9834a8a97e35bc0861b3d28039421
Ashweej Pajithaya <ashweejpajithaya@gmail.com>
2026-05-05 11:45:48 +0530

    Implement bubble sort and validate k for kth largest
    Replaced sorting method with bubble sort and added validation for k.

--- kthlarge.java
+++ kthlarge.java
@@ -1,6 +1,4 @@
-import java.util.Arrays;
 import java.util.Scanner;
-
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -13,7 +11,20 @@ class Main {
         }
         System.out.print("Enter k : ");
         int k = sc.nextInt();
-        Arrays.sort(numbers);
-        System.out.println("The " + k + "-th largest element is: " + numbers[numbers.length - k]);
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (numbers[j] > numbers[j + 1]) {
+                    // Swap elements
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                }
+            }
+        }
+        if (k > 0 && k <= n) {
+            System.out.println("The " + k + "-th largest element is: " + numbers[numbers.length - k]);
+        } else {
+            System.out.println("Invalid value of k.");
+        }
     }
 }

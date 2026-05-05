1f24139543939f884aecee8e9755d300a5059b70
Ashweej Pajithaya <ashweejpajithaya@gmail.com>
2026-05-05 15:11:15 +0530

    Add palindrome string reversal solution

---
+++ palinstr.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+class Solution {
+    public static void main(String[] args) {
+    StringBuilder sb = new StringBuilder();
+    char[] s = {'h', 'e', 'l', 'e', 'o'};
+    char[] o = s;
+    System.out.println(Arrays.toString(s));
+    new Solution().reverseString(s);
+    System.out.println(Arrays.toString(s));
+    if (Arrays.equals(o,s)){
+        System.out.println("True");
+    }else{
+        System.out.println("False");
+    }
+}
+    public void reverseString(char[] s) {
+        int start = 0;
+        int end = s.length - 1;
+        while(start <= end){
+            char temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
